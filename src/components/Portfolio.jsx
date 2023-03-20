bff57fd76601a50100853e8941a4d722b292808a
Mehmet GÜVEN <guvennmehmet@gmail.com>
2023-03-20 16:48:36 +0300

    positioning done.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,7 +5,7 @@ import navbar from "../assets/portfolio/navbar.jpg";
 import reactParallax from "../assets/portfolio/reactParallax.jpg";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
-import usestate from "../assets/portfolio/usestate.jpg";
+
 
 const Portfolio = () => {
 
@@ -33,16 +33,12 @@ const Portfolio = () => {
     {
       id:6,
       src: reactWeather
-    },
-    {
-      id:7,
-      src: usestate
     }
   ]
 
   return (
     <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-screen">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
           <p className="py-8">Check out some of my work right here</p>
